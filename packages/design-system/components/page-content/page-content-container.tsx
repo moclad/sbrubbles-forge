faba86c58a3749531a269328d2b37df2d62ec58c
Beno <sbrubbles@outlook.de>
2025-04-17 23:26:44 +0200

    remove semver

--- packages/design-system/components/page-content/page-content-container.tsx
+++ packages/design-system/components/page-content/page-content-container.tsx
@@ -9,7 +9,7 @@ export function PageContentContainer({
   children,
 }: Readonly<PageContentContainerProps>) {
   return (
-    <div className='min-h-[100vh] flex-1 rounded-sm bg-muted/50 p-2 md:min-h-min'>
+    <div className='min-h-[100vh] flex-1 rounded-sm border p-2 md:min-h-min'>
       {children}
     </div>
   );
