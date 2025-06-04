8e83b77868243e163035492f89bd4372acfefbc7
Beno <sbrubbles@outlook.de>
2025-06-04 22:59:21 +0200

    2FA Authentication

--- apps/app/app/[locale]/(unauthenticated)/two-factor/[[...two-factor]]/page.tsx
+++ apps/app/app/[locale]/(unauthenticated)/two-factor/[[...two-factor]]/page.tsx
@@ -1,8 +1,8 @@
+import { TwoFactorForm } from '@repo/auth/components/auth/two-factor-form';
 import { getI18n } from '@repo/localization/i18n/server';
 import { createMetadata } from '@repo/seo/metadata';
 
 import type { Metadata } from 'next';
-
 const title = 'Welcome back';
 const description = 'Enter your details to sign in.';
 export const metadata: Metadata = createMetadata({ title, description });
@@ -10,7 +10,7 @@ export const metadata: Metadata = createMetadata({ title, description });
 const SignInPage = async () => {
   const t = await getI18n();
 
-  return <>Two Factor</>;
+  return <TwoFactorForm />;
 };
 
 export default SignInPage;
