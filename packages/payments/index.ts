cbb7fc9bc3a9cdc1305e1a786a1e59825656de9e
Beno <sbrubbles@outlook.de>
2026-04-12 21:55:33 +0200

    fix build

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 import { keys } from './keys';
 
 export const stripe = new Stripe(keys().STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2026-02-25.clover',
+  apiVersion: '2026-03-25.dahlia',
 });
 
 export type { Stripe } from 'stripe';
