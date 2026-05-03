a9ed6489629482a6d7af112cfe76293fb3df2739
Sbrubbles <sbrubbles@outlook.de>
2026-05-03 23:11:39 +0200

    changes ok. Update packages

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 import { keys } from './keys';
 
 export const stripe = new Stripe(keys().STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2026-03-25.dahlia',
+  apiVersion: '2026-04-22.dahlia',
 });
 
 export type { Stripe } from 'stripe';
